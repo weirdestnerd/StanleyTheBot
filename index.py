05096cab69cb4c733a812241422cb2840803be63
weirdestnerd <olu1995@hotmail.com>
2017-01-02 22:30:31 -0700

    Fixed quotation marks in 'imageUrl'

--- index.py
+++ index.py
@@ -13,9 +13,9 @@ def index():
 
 @app.route("/analyize", methods=['GET', 'POST'])
 def img_upload():
-    #imageUrl = request.form['hiddenUrl']
-    imageUrl = 'https://firebasestorage.googleapis.com/v0/b/stanleythebot-8c6b0.appspot.com/o/images%2Falan-frog.jpg?alt=media&token=07b803f8-388d-43f6-bee3-db00e098a05f'
-    regc = json.dumps(visual_recognition.classify(images_url=imageUrl), indent=2)
-    return render_template('info.html', regc=regc, imgUrl=imageUrl)
+    imageUrl = request.form['hiddenUrl']
+    regc = json.dumps(visual_recognition.classify(images_url=str(imageUrl)), indent=2)
+    return render_template('info.html', regc=regc, imgURL=str(imageUrl))
+
 if __name__ == "__main__":
     app.run()
