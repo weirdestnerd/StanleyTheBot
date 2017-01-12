0bb7431eaa1583dcd67c6dc40e3c595ca2ca377b
weirdestnerd <olu1995@hotmail.com>
2017-01-12 11:47:36 -0700

    Improved bot response: recognizes famous people and classifies other people according to age

--- index.py
+++ index.py
@@ -24,39 +24,24 @@ def get_user_name():
 def img_upload():
     #get the given image's url (firebase storage url)
     imageUrl = str(request.form['hiddenUrl'])
+
     #IBM Watson classifies image and returns a dictionary
+    #TODO: use try catch here, in case of an error
+    #IBM Watson checks for faces in the photo
     result_from_IBM_Watson = visual_recognition.detect_faces(images_url=imageUrl)
     list_of_faces = result_from_IBM_Watson['images'][0]['faces']
+    #if faces are found in the image
     if list_of_faces:
-        bot_say = parse_faces(list_of_faces)
+        bot_say = "I can see " + parse_faces(list_of_faces) + " in this photo."
     else:
+        #TODO: use try catch here, in case of an error
         result_from_IBM_Watson = visual_recognition.classify(images_url=imageUrl)
+
         #get list of dictionaries containing 'scores' and 'class' from 'result_from_IBM_Watson'
         list_of_scores_and_classes = result_from_IBM_Watson['images'][0]['classifiers'][0]['classes']
-        #get rid of classes with more than one word
-        list_of_scores_and_classes = [dictionary for dictionary in list_of_scores_and_classes if " " not in dictionary['class']]
-        #extract nouns in the list of scores and classes
-        for dictionary in list_of_scores_and_classes:
-            s = wordnet.synsets(dictionary['class'])[0]
-            if s.pos != "NN":
-                del list_of_scores_and_classes[list_of_scores_and_classes.index(dictionary)]
-        #get rid of words that are plural/singular of other words
-        for dictionary in list_of_scores_and_classes:
-            #get index of the current dictionary
-            x = list_of_scores_and_classes.index(dictionary) + 1
-            #go thru the rest of the list from the index of the current dictionary
-            while x != len(list_of_scores_and_classes):
-                #is the current dictionary the same as the plural or singular of other dictionarys?
-                if dictionary['class'] == pluralize(list_of_scores_and_classes[x]['class']) or dictionary['class'] == singularize(list_of_scores_and_classes[x]['class']):
-                    #if yes, which dictionary has higher score (greater chance of predicting that the dictionary is the image)
-                    #remove dictionary with lower score
-                    if int(dictionary['score']) > int(list_of_scores_and_classes[x]['score']):
-                        del list_of_scores_and_classes[x]
-                    #if current dictionary has lower score, delete it and move to next dictionary
-                    else:
-                        del list_of_scores_and_classes[list_of_scores_and_classes.index(dictionary)]
-                        continue
-                x += 1
+        #refine the result from IBM Watson
+        list_of_scores_and_classes = refine_classes(list_of_scores_and_classes)
+
         bot_say = parse_classify(list_of_scores_and_classes)
     return render_template('analyze.html', bot_say = bot_say, imgURL=imageUrl)
 
@@ -64,13 +49,63 @@ def parse_faces(list_of_faces):
     '''
     parsing list_of_faces['age'], list_of_faces['gender'], list_of_faces['identity']
     '''
-    return "There's a " + list_of_faces[0]['gender']['gender'] + " betweens ages " + str(list_of_faces[0]['age']['max']) + " and " + str(list_of_faces[0]['age']['min'])
+    faces_found = []
+
+    #list_of_faces is a list of dictionaries containing info about the different face(s) found in the image
+    #go thru each dictionaries in the list
+    for x in range(0, len(list_of_faces)):
+        #if the current dictionary has key 'identity'
+        if 'identity' in list_of_faces[x]:
+            #add that 'famous' person to faces_found
+            faces_found.append(list_of_faces[x]['identity']['name'])
+        else:
+            #if current dictionary is not representing a 'famous' person
+            #then, use age range to classify the male or female
+            faces_found.append(classify_age(list_of_faces[x]['age']['max'], list_of_faces[x]['age']['min'], list_of_faces[x]['gender']['gender']))
+    return ", ".join(faces_found)
 
 def parse_classify(list_of_scores_and_classes):
-    string = ""
+    classes_in_string = ", ".join(dictionary['class'] for dictionary in list_of_scores_and_classes)
+    return "Here's what I see the this photo: " + classes_in_string
+
+def classify_age(age_max, age_min, gender):
+    if age_min < 12:
+        return "a little boy" if gender == "MALE" else "a little girl"
+    elif age_min >= 12 and age_max <= 17:
+        return "a teenage boy" if gender == "MALE" else "a teenage girl"
+    elif age_min >= 18 and age_max <= 24:
+        return "a young guy" if gender == "MALE" else "a young lady"
+    elif age_min >= 25 and age_max <= 35:
+        return "a young man" if gender == "MALE" else "a young woman"
+    else:
+        return "a old man" if gender == "MALE" else "a old woman"
+
+def refine_classes(list_of_scores_and_classes):
+    #get rid of classes with more than one word
+    list_of_scores_and_classes = [dictionary for dictionary in list_of_scores_and_classes if " " not in dictionary['class']]
+    #extract nouns in the list of scores and classes
     for dictionary in list_of_scores_and_classes:
-        string = string + ", " + dictionary['class']
-    return "there is " + string + " in this photo"
+        s = wordnet.synsets(dictionary['class'])[0]
+        if s.pos != "NN":
+            del list_of_scores_and_classes[list_of_scores_and_classes.index(dictionary)]
+    #get rid of words that are plural/singular of other words
+    for dictionary in list_of_scores_and_classes:
+        #get index of the current dictionary
+        x = list_of_scores_and_classes.index(dictionary) + 1
+        #go thru the rest of the list from the index of the current dictionary
+        while x != len(list_of_scores_and_classes):
+            #is the current dictionary the same as the plural or singular of other dictionarys?
+            if dictionary['class'] == pluralize(list_of_scores_and_classes[x]['class']) or dictionary['class'] == singularize(list_of_scores_and_classes[x]['class']):
+                #if yes, which dictionary has higher score (greater chance of predicting that the dictionary is the image)
+                #remove dictionary with lower score
+                if int(dictionary['score']) > int(list_of_scores_and_classes[x]['score']):
+                    del list_of_scores_and_classes[x]
+                #if current dictionary has lower score, delete it and move to next dictionary
+                else:
+                    del list_of_scores_and_classes[list_of_scores_and_classes.index(dictionary)]
+                    continue
+            x += 1
+    return list_of_scores_and_classes
 
 if __name__ == "__main__":
     app.run()
